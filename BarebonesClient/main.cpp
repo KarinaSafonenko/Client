03a2c74325cdd90e1a154b836798a9e90f7ca700
Karina <karina.safonenko@gmail.com>
2017-11-18 23:23:39 +0300

    Working client

--- BarebonesClient/main.cpp
+++ BarebonesClient/main.cpp
@@ -7,9 +7,8 @@
 using namespace std;
 
 void main() {
-	string ipAdress = "127.0.0.1";  // IP Adress сервера
+	string ipAdress = "192.168.0.103";  // IP Adress сервера
 	int port = 5223;				//Порт сервера
-	//string str = "Darling, you look perfect";
 
 	//Инциализируем винсокет
 	WSADATA data;
@@ -54,34 +53,31 @@ void main() {
 	string userInput;
 	int random = (rand() % 10 + 2) * 1000;
 
-	//do {
-		//Ждем ввода инфы
-		cout << ">";
-		getline(cin, userInput);
-		Sleep(random);
-		if (userInput.size() > 0) {				//Если что-то введено
+	//Ждем ввода информации
+	cout << ">";
+	getline(cin, userInput);
+	Sleep(random);
+	if (userInput.size() > 0) {				//Если что-то введено
 
 			//Отправляем текст
 
-			int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
-			if (sendResult != SOCKET_ERROR) {
+		int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+		if (sendResult != SOCKET_ERROR) {
 
 				//Ждем ответа
-				ZeroMemory(buff, 4096);
+			ZeroMemory(buff, 4096);
 
-				int bytesReceived = recv(sock, buff, 4096, 0);
-				if (bytesReceived > 0) {
-					//Выводим строку
-					cout << "SERVER>" << string(buff, 0, bytesReceived) << endl;
-				}
+			int bytesReceived = recv(sock, buff, 4096, 0);
+			if (bytesReceived > 0) {
+
+				cout << "SERVER>" << string(buff, 0, bytesReceived) << endl;
 			}
 		}
+	}
 
-	//} while (userInput.size() != 0);
+	random = (rand() % 10 + 2) * 1000;
+	Sleep(random);
 
-		random = (rand() % 10 + 2) * 1000;
-		Sleep(random);
-	// Close down everything
 	closesocket(sock);
 	WSACleanup();
 }
